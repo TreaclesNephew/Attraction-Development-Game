a9dd4538ed63c56eb70f9bc2e0788232a3835acb
TreaclesNephew <alex.boulting@hotmail.co.uk>
2023-02-16 15:47:29 +0000

    Back to the main

--- Main.py
+++ Main.py
@@ -1,84 +1,68 @@
+import csv
 import tkinter as tk
 from tkinter import ttk
 
-# Define a list of prefabricated rides
-ride_options = [
-    {"name": "Roller Coaster", "cost": 50},
-    {"name": "Ferris Wheel", "cost": 20},
-    {"name": "Water Slide", "cost": 35},
-    {"name": "Carousel", "cost": 15}
-]
+class Ride:
+    def __init__(self, tech, rtype, vtype, name, capacity, max_speed, max_height, max_length, max_gforce):
+        self.tech = tech
+        self.type = rtype
+        self.vehicle_type = vtype
+        self.name = name
+        self.capacity = capacity
+        self.max_speed = max_speed
+        self.max_height = max_height
+        self.max_length = max_length
+        self.max_gforce = max_gforce
 
-# Set up the initial game state
-budget = 100
-rides = []
+def read_ride_data():
+    ride_list = []
+    with open('ride_data.csv', 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)  # Skip header row
+        for row in reader:
+            tech = row[0]
+            rtype = row[1]
+            vtype = row[2]
+            name = row[3]
+            capacity = row[4]
+            max_speed = row[5]
+            max_height = row[6]
+            max_length = row[7]
+            max_gforce = row[8]
+            ride_list.append(Ride(tech, rtype, vtype, name, capacity, max_speed, max_height, max_length, max_gforce))
+    return ride_list
 
-# Create the main window
-root = tk.Tk()
-root.title("Attraction Manufacturing Company Game")
+def display_ride_data(ride_list):
+    root = tk.Tk()
+    root.title('Ride Data')
 
-# Create a label to display the player's budget
-budget_label = tk.Label(root, text=f"Budget: ${budget}")
-budget_label.pack()
+    table = ttk.Treeview(root, columns=('Technology', 'Type', 'Vehicle Type', 'Name', 'Capacity', 'Max Speed (m/s)', 'Max Height (m)', 'Max Length (m)', 'Max G-Force'))
+    table.heading('#0', text='Index')
+    table.heading('Technology', text='Technology')
+    table.heading('Type', text='Type')
+    table.heading('Vehicle Type', text='Vehicle Type')
+    table.heading('Name', text='Name')
+    table.heading('Capacity', text='Capacity')
+    table.heading('Max Speed (m/s)', text='Max Speed (m/s)')
+    table.heading('Max Height (m)', text='Max Height (m)')
+    table.heading('Max Length (m)', text='Max Length (m)')
+    table.heading('Max G-Force', text='Max G-Force')
+    table.column('#0', width=50)
+    table.column('Technology', width=120)
+    table.column('Type', width=120)
+    table.column('Vehicle Type', width=120)
+    table.column('Name', width=120)
+    table.column('Capacity', width=120)
+    table.column('Max Speed (m/s)', width=120)
+    table.column('Max Height (m)', width=120)
+    table.column('Max Length (m)', width=120)
+    table.column('Max G-Force', width=120)
+    table.grid(row=0, column=0)
 
-# Create a listbox to display the player's rides
-rides_label = tk.Label(root, text="Rides:")
-rides_label.pack()
-ride_listbox = tk.Listbox(root)
-ride_listbox.pack()
+    for i, ride in enumerate(ride_list):
+        table.insert('', 'end', text=i, values=(ride.tech, ride.type, ride.vehicle_type, ride.name, ride.capacity, ride.max_speed, ride.max_height, ride.max_length, ride.max_gforce))
 
-# Create a frame for the ride customization controls
-custom_frame = tk.Frame(root)
-custom_frame.pack()
+    root.mainloop()
 
-# Create a label and entry box for the ride name
-name_label = tk.Label(custom_frame, text="Ride Name:")
-name_label.grid(row=0, column=0)
-name_entry = tk.Entry(custom_frame)
-name_entry.grid(row=0, column=1)
-
-# Create a label and entry box for the ride cost
-cost_label = tk.Label(custom_frame, text="Ride Cost:")
-cost_label.grid(row=1, column=0)
-cost_entry = tk.Entry(custom_frame)
-cost_entry.grid(row=1, column=1)
-
-# Create a dropdown menu to select a prefabricated ride
-ride_var = tk.StringVar()
-ride_var.set(ride_options[0]["name"])  # Set default option to first ride in the list
-ride_menu = ttk.OptionMenu(custom_frame, ride_var, *list(map(lambda x: x["name"], ride_options)))
-ride_menu.grid(row=2, column=0)
-
-# Create a button to buy a new ride
-def buy_ride():
-    global budget
-    global rides
-    ride_cost = int(cost_entry.get())
-    if budget >= ride_cost:
-        budget -= ride_cost
-        new_ride = name_entry.get() if name_entry.get() else ride_var.get()
-        rides.append((new_ride, ride_cost))
-        ride_listbox.insert(tk.END, f"{new_ride} (${ride_cost})")
-        budget_label["text"] = f"Budget: ${budget}"
-
-buy_button = tk.Button(custom_frame, text="Buy new ride", command=buy_ride)
-buy_button.grid(row=2, column=1)
-
-# Create a button to remove a ride
-def remove_ride():
-    global budget
-    global rides
-    selected_ride = ride_listbox.curselection()
-    if selected_ride:
-        selected_ride = selected_ride[0]
-        ride_to_remove = rides[selected_ride]
-        rides.pop(selected_ride)
-        ride_listbox.delete(selected_ride)
-        budget += int(ride_to_remove[1] * 0.5)
-        budget_label["text"] = f"Budget: ${budget}"
-
-remove_button = tk.Button(custom_frame, text="Remove ride", command=remove_ride)
-remove_button.grid(row=3, column=0)
-
-# Run the main event loop
-root.mainloop()
+ride_list = read_ride_data()
+display_ride_data(ride_list)
